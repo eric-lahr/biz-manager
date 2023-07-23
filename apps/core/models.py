938f0f1f107df83721187774fc87d2bf2d6be71e
tuffgong71 <evlahr@outlook.com>
2023-07-23 09:58:05 -0700

    added imports to models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
+from phone_field import PhoneField
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 # Create your models here.
